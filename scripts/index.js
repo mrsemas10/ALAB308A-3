f7b617c3dbe441f0190634caeed772a85f13a9be
mrsemas10 <mrsemas10@gmail.com>
2024-07-22 22:39:44 -0400

    lab done

--- scripts/index.js
+++ scripts/index.js
@@ -3,42 +3,30 @@ console.log("running")
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-// function getUserData(id) {
-//   const dbs = {
-//     db1: db1,
-//     db2: db2,
-//     db3: db3
-//   };
-// }
+function getUserData(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3
+  };
+}
 
 async function getUser(id) {
+  const returnedValue = await central(id);
+  console.log(returnedValue);
 
-  const dbNum = await central(id);
-
-  console.log(dbNum);
-
-  // const value = await vault(id);
-
-  // console.log(value);
-
- 
-
-  new Promise(function (resolve, reject) {
-
-    vault(id).then((returnedValue) => {
-
+ const dbPromis = new Promise((resolve, reject) => {
+  vault(id).then((returnedValue) => {
       console.log(returnedValue);
-
-      // let userData = returnedValue;
-
-    });
-
-    resolve("Data received"); // when successful
-
-    reject("Id Invalid"); // when error
-
-  });
+      });
+  setTimeout(() => {
+    resolve("received data");
+    reject("did not receive");
+  }, 2000);
+ }) 
 }
 
 
-console.log(getUser(1));
+console.log(getUser(8));
+// console.log(getUser(true));
+// console.log(getUser(11));
